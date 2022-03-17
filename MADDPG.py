f98d6e99ac95669b203a113d6c4a38e543a63422
Git-123-Hub <2063487599@qq.com>
2022-03-17 20:55:58 +0800

    add buffer; add logger;

--- MADDPG.py
+++ MADDPG.py
@@ -1,16 +1,36 @@
+import logging
+
 from Agent import Agent
+from buffer import buffer
+
+
+def setup_logger(filename):
+    """ set up logger with filename. """
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    handler = logging.FileHandler(filename, mode='w')
+    handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s--%(levelname)s--%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+    return logger
 
 
 class MADDPG:
     def __init__(self, obs_dim_list, act_dim_list, capacity, batch_size, actor_lr, critic_lr):
         # sum all the dims of each agent to get input dim for critic
         global_obs_dim = sum(obs_dim_list + act_dim_list)
-        # create all the agents
+        # create all the agents and corresponding replay buffer
         self.agents = []
+        self.buffers = []
         for obs_dim, act_dim in zip(obs_dim_list, act_dim_list):
             self.agents.append(Agent(obs_dim, act_dim, global_obs_dim, actor_lr, critic_lr))
-        # create all the buffers
-        self.buffers = []
+            self.buffers.append(buffer(capacity, obs_dim, act_dim))
+        self.batch_size = batch_size
+        self.logger = setup_logger('maddpg.log')
 
     def add(self):
         """add experience to buffer"""
---
+++ buffer.py
@@ -0,0 +1,60 @@
+import numpy as np
+import torch
+
+
+class buffer:
+    def __init__(self, capacity, obs_dim, act_dim):
+        self.capacity = capacity
+
+        self.obs = np.zeros((capacity, obs_dim))
+        self.action = np.zeros((capacity, act_dim))
+        self.reward = np.zeros(capacity)
+        self.next_obs = np.zeros((capacity, obs_dim))
+        self.done = np.zeros(capacity, dtype=bool)
+
+        self._index = 0
+        self._size = 0
+
+    def add(self, state, action, reward, next_state, done):
+        """ add an experience to the memory """
+        self.obs[self._index] = state
+        self.action[self._index] = action
+        self.reward[self._index] = reward
+        self.next_obs[self._index] = next_state
+        self.done[self._index] = done
+
+        self._index = (self._index + 1) % self.capacity
+        if self._size < self.capacity:
+            self._size += 1
+
+    def sample(self, indices):
+        # retrieve data, Note that the data stored is ndarray
+        obs = self.obs[indices]
+        action = self.action[indices]
+        reward = self.reward[indices]
+        next_obs = self.next_obs[indices]
+        done = self.done[indices]
+
+        def transfer(data, first_dim=False):
+            """
+            transfer ndarray to torch.tensor,
+            if `first_dim` is True, stack the ndarray so that the first dimension is `batch_size`,
+            otherwise, the returned value is just a tensor with length of the original ndarray.
+            """
+            if first_dim:
+                data = np.vstack(data)
+            data = torch.from_numpy(data).float()
+            return data
+
+        # NOTE that `obs`, `action`, `next_obs` will be passed to network(nn.Module),
+        # so the first dimension should be `batch_size`
+        obs = transfer(obs, first_dim=True)  # torch.Size([batch_size, state_dim])
+        action = transfer(action, first_dim=True)  # torch.Size([batch_size, action_dim])
+        reward = transfer(reward)  # just a tensor with length: batch_size
+        next_obs = transfer(next_obs, first_dim=True)  # Size([batch_size, state_dim])
+        done = transfer(done)  # just a tensor with length: batch_size
+
+        return obs, action, reward, next_obs, done
+
+    def __len__(self):
+        return self._size
